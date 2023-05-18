b8672acb2f8b0267ad2057bcf093c483f803c41e
seba.developer <cmoralesaroca@gmail.com>
2023-05-18 01:55:02 -0400

    Estoy preparando los botones para que el contenido no se duplique

--- src/js/app/adminUser.js
+++ src/js/app/adminUser.js
@@ -1,26 +1,75 @@
-function funUserAdmin() {
-    let mainMenu = document.getElementById('mainMenu');
-        let menu1 = document.createElement('button');
-            mainMenu.appendChild(menu1);
-            menu1.className ='menuBtn';
-            menu1.id = 'menu1';
-            menu1.innerHTML ="Look Users"
-            let img1 = document.createElement('img');
+const exitHandler = require("npm/lib/utils/exit-handler");
+
+    function funUserAdmin() {
+        let mainMenu = document.getElementById('mainMenu');
+            let menu1 = document.createElement('button');
+                mainMenu.appendChild(menu1);
+                menu1.className ='menuBtn';
+                menu1.id ='menu1';
+                menu1.innerHTML ="Look Users"
+                let img1 = document.createElement('img');
                 menu1.appendChild(img1);
                 img1.className ='menuImg';
                 img1.src = imgUsers;
-        let menu2 = document.createElement('button');
-            mainMenu.appendChild(menu2);
-            menu2.className ='menuBtn';
-            menu2.id ='menu2';
-            menu2.innerHTML ="Create user";
-            let img2 = document.createElement('img');
-                menu2.appendChild(img2);
-                img2.className ='menuImg';
-                img2.src = imgAddUsers;
-    menu1.addEventListener('click', ()=>{
-        let text1 = document.createElement('p');
-        mainDashboard.appendChild(text1);
-        text1.innerHTML =users;
-        })
-}
+                let menu2 = document.createElement('button');
+                mainMenu.appendChild(menu2);
+                menu2.className ='menuBtn';
+                menu2.id ='menu2';
+                menu2.innerHTML ="Create user";
+                let img2 = document.createElement('img');
+                    menu2.appendChild(img2);
+                    img2.className ='menuImg';
+                    img2.src = imgAddUsers;
+        const contLoginNewUser = document.createElement('div');
+        const contLookUsers = document.createElement('div');
+        let x = true;
+        let z = true;
+        menu1.addEventListener('click',()=>{
+            if (x === true) {
+                mainDashboard.appendChild(contLookUsers);
+                contLookUsers.className='contLookUsers';
+                contLookUsers.id='contLookUsers';
+            let textLookUsers = document.createElement('p');
+                contLookUsers.appendChild(textLookUsers);
+                textLookUsers.innerHTML = users;
+            contLoginNewUser.remove();
+            x = false;
+            }
+            })
+        menu2.addEventListener('click',()=>{
+                mainDashboard.appendChild(contLoginNewUser);
+                contLoginNewUser.className='contLoginNewUser';
+            let inputFirstName = document.createElement('input');
+                contLoginNewUser.appendChild(inputFirstName);
+                inputFirstName.type='text';
+                inputFirstName.placeholder="  First Name";
+                inputFirstName.className='inputFirstName';
+            let inputLastName = document.createElement('input');
+                    contLoginNewUser.appendChild(inputLastName);
+                    inputLastName.type='text';
+                    inputLastName.placeholder="  Last Name";
+                    inputLastName.className='inputLastName';
+                let inputPositions = document.createElement('input');
+                    contLoginNewUser.appendChild(inputPositions);
+                    inputPositions.type='text';
+                    inputPositions.placeholder="  Position";
+                    inputPositions.className='inputPositions';
+                    let inputSexo = document.createElement('input');
+                    contLoginNewUser.appendChild(inputSexo);
+                    inputSexo.type='text';
+                    inputSexo.placeholder="  Sexo";
+                    inputSexo.className='inputSexo';
+                let inputId = document.createElement('input');
+                    contLoginNewUser.appendChild(inputId);
+                    inputId.type='password';
+                    inputId.placeholder="  ID";
+                    inputId.className='inputId';
+                    let btnPushUser = document.createElement('button');
+                    contLoginNewUser.appendChild(btnPushUser);
+                    btnPushUser.type='submit';
+                    btnPushUser.className='btnPushUser';
+                    btnPushUser.innerHTML='Create User';
+                contLookUsers.remove();
+                x = true;
+            })
+            }
--- src/js/template/userTempl.js
+++ src/js/template/userTempl.js
@@ -4,92 +4,93 @@ function funMain(h,j,k) {
     mainContPrin.className ='mainContPrin';
 
         const mainMenu = document.createElement('div');
-        mainContPrin.appendChild(mainMenu);
-        mainMenu.className ='mainMenu';
-        mainMenu.id ='mainMenu';
+            mainContPrin.appendChild(mainMenu);
+            mainMenu.className ='mainMenu';
+            mainMenu.id ='mainMenu';
 
         const mainDivPrin = document.createElement('div');
-        mainContPrin.appendChild(mainDivPrin);
-        mainDivPrin.className ='mainDivPrin';
+            mainContPrin.appendChild(mainDivPrin);
+            mainDivPrin.className ='mainDivPrin';
+            mainDivPrin.id='mainDivPrin';
 
             const mainWelcome = document.createElement('div');
-            mainDivPrin.appendChild(mainWelcome);
-            mainWelcome.className ='mainWelcome';
+                mainDivPrin.appendChild(mainWelcome);
+                mainWelcome.className ='mainWelcome';
 
                 let mainWelcomeTitle = document.createElement('h2');
-                mainWelcome.appendChild(mainWelcomeTitle);
-                mainWelcomeTitle.className ='mainWelcomeTitle';
-                mainWelcomeTitle.id ='mainWelcomeTitle';
-                mainWelcomeTitle.innerHTML = "Hello "+h+"!";
+                    mainWelcome.appendChild(mainWelcomeTitle);
+                    mainWelcomeTitle.className ='mainWelcomeTitle';
+                    mainWelcomeTitle.id ='mainWelcomeTitle';
+                    mainWelcomeTitle.innerHTML = "Hello "+h+"!";
 
                 let mainWelcomeText = document.createElement('p');
-                mainWelcome.appendChild(mainWelcomeText);
-                mainWelcomeText.className ='mainWelcomeText';
-                mainWelcomeText.id ='mainWelcomeText';
-                mainWelcomeText.innerHTML = j;
+                    mainWelcome.appendChild(mainWelcomeText);
+                    mainWelcomeText.className ='mainWelcomeText';
+                    mainWelcomeText.id ='mainWelcomeText';
+                    mainWelcomeText.innerHTML = j;
 
                 let mainWelcomeImg = document.createElement('img');
-                mainWelcome.appendChild(mainWelcomeImg);
-                mainWelcomeImg.className ='mainWelcomeImg';
-                mainWelcomeImg.id ='mainWelcomeImg';
-                mainWelcomeImg.src = k;
+                    mainWelcome.appendChild(mainWelcomeImg);
+                    mainWelcomeImg.className ='mainWelcomeImg';
+                    mainWelcomeImg.id ='mainWelcomeImg';
+                    mainWelcomeImg.src = k;
 
             const mainDashboard = document.createElement('div');
-            mainDivPrin.appendChild(mainDashboard);
-            mainDashboard.className ='mainDashboard';
-            mainDashboard.id ='mainDashboard';
+                mainDivPrin.appendChild(mainDashboard);
+                mainDashboard.className ='mainDashboard';
+                mainDashboard.id ='mainDashboard';
 
         const mainDivSecond = document.createElement('div');
-        mainContPrin.appendChild(mainDivSecond);
-        mainDivSecond.className ='mainDivSecond';
+            mainContPrin.appendChild(mainDivSecond);
+            mainDivSecond.className ='mainDivSecond';
 
             const secondCalificatios = document.createElement('div');
-            mainDivSecond.appendChild(secondCalificatios);
-            secondCalificatios.className ='secondCalificatios';
+                mainDivSecond.appendChild(secondCalificatios);
+                secondCalificatios.className ='secondCalificatios';
 
             const secondNotes = document.createElement('div');
-            mainDivSecond.appendChild(secondNotes);
-            secondNotes.className ='secondNotes'
+                mainDivSecond.appendChild(secondNotes);
+                secondNotes.className ='secondNotes'
 
                 const notesInput =  document.createElement('input');
-                secondNotes.appendChild(notesInput);
-                notesInput.className ='notesInput';
-                notesInput.innerHTML ="Write your text";
+                    secondNotes.appendChild(notesInput);
+                    notesInput.className ='notesInput';
+                    notesInput.innerHTML ="Write your text";
 
                 const notesBtn = document.createElement('button');
-                secondNotes.appendChild(notesBtn);
-                notesBtn.className ='notesBtn';
-                notesBtn.innerHTML ="Add Personal Note";
+                    secondNotes.appendChild(notesBtn);
+                    notesBtn.className ='notesBtn';
+                    notesBtn.innerHTML ="Add Personal Note";
 
                     notesBtn.addEventListener('click',()=>{
                         console.log("Nota Agregada");
 
                         let newNotes = document.createElement('div');
-                        secondNotes.appendChild(newNotes);
-                        newNotes.className ='newNotes';
+                            secondNotes.appendChild(newNotes);
+                            newNotes.className ='newNotes';
 
                             let checkNotes = document.createElement('input');
-                            newNotes.appendChild(checkNotes);
-                            checkNotes.type ='checkbox';
-                            checkNotes.className ='ckeckNotes';
+                                newNotes.appendChild(checkNotes);
+                                checkNotes.type ='checkbox';
+                                checkNotes.className ='ckeckNotes';
 
                                 let removeNotes = document.createElement('button');
-                                newNotes.appendChild(removeNotes);
-                                removeNotes.className ='removeNotes';
-                                removeNotes.id = 'removeNotes';
+                                    newNotes.appendChild(removeNotes);
+                                    removeNotes.className ='removeNotes';
+                                    removeNotes.id = 'removeNotes';
 
                                     let imgNotes = document.createElement('img');
-                                    removeNotes.appendChild(imgNotes);
-                                    imgNotes.className ='imgNotes';
-                                    imgNotes.src = imgRemove;
+                                        removeNotes.appendChild(imgNotes);
+                                        imgNotes.className ='imgNotes';
+                                        imgNotes.src = imgRemove;
 
                                     removeNotes.addEventListener('click',()=>{
                                         newNotes.remove();
                                     })
 
                                     let textNotes = document.createElement('p');
-                                    newNotes.appendChild(textNotes);
-                                    textNotes.className ='textNotes';
-                                    textNotes.innerHTML =notesInput.value;
+                                        newNotes.appendChild(textNotes);
+                                        textNotes.className ='textNotes';
+                                        textNotes.innerHTML =notesInput.value;
                     })
 }
