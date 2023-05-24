0698ba1612bfdb3d56c531360d717d679e079afb
seba.developer <cmoralesaroca@gmail.com>
2023-05-24 16:01:47 -0400

    Estroy trabajando en la seccion de teacher, especificamente creando el formulario de califications

--- src/js/app/adminUser.js
+++ src/js/app/adminUser.js
@@ -1,28 +1,30 @@
 function funUserAdmin() {
     let mainMenu = document.getElementById('mainMenu');
         let menu1 = document.createElement('button');
-            mainMenu.appendChild(menu1);
-            menu1.className ='menuBtn';
-            menu1.id ='menu1';
-            menu1.innerHTML ="Look Users"
+        mainMenu.appendChild(menu1);
+        menu1.className ='menuBtn';
+        menu1.id ='menu1';
+        menu1.innerHTML ="Look Users"
             let img1 = document.createElement('img');
             menu1.appendChild(img1);
             img1.className ='menuImg';
             img1.src = imgUsers;
-            let menu2 = document.createElement('button');
-            mainMenu.appendChild(menu2);
-            menu2.className ='menuBtn';
-            menu2.id ='menu2';
-            menu2.innerHTML ="Create user";
+        let menu2 = document.createElement('button');
+        mainMenu.appendChild(menu2);
+        menu2.className ='menuBtn';
+        menu2.id ='menu2';
+        menu2.innerHTML ="Create user";
             let img2 = document.createElement('img');
-                menu2.appendChild(img2);
-                img2.className ='menuImg';
-                img2.src = imgAddUsers;
-    const contListUsers = document.createElement('div');
+            menu2.appendChild(img2);
+            img2.className ='menuImg';
+            img2.src = imgAddUsers;
+        const contDiv = document.createElement('div');
         users.forEach(user => {
+            const contListUsers = document.createElement('div');
             let listUsers = document.createTextNode(`Nombre:${user.firstname}`);
             menu1.addEventListener('click',()=>{
-                mainDashboard.appendChild(contListUsers);
+                mainDashboard.appendChild(contDiv);
+                contDiv.appendChild(contListUsers);
                 contListUsers.appendChild(listUsers);
                 contLoginNewUser.remove();
         });
@@ -63,6 +65,11 @@ function funUserAdmin() {
         btnPushUser.type='submit';
         btnPushUser.className='btnPushUser';
         btnPushUser.innerHTML='Create User';
-        contListUsers.remove();
+        mainDashboard.removeChild(contDiv);
+            btnPushUser.addEventListener('click',()=>{
+                let usuarioNuevo = new userName(inputFirstName.value, inputLastName.value, inputPositions.value, inputSexo.value, inputId.value);
+                users.push(usuarioNuevo);
+                console.log(users);
+            })
     })
 }
--- src/js/app/teacherUser.js
+++ src/js/app/teacherUser.js
@@ -1,66 +1,97 @@
 function funUserTeacher() {
-    let mainMenu = document.getElementById('mainMenu');
-        let menu1 = document.createElement('button');
-        mainMenu.appendChild(menu1);
-        menu1.className ='menuBtn';
-        menu1.id = 'menu1';
-        menu1.innerHTML ="Califications"
-            let img1 = document.createElement('img');
-            menu1.appendChild(img1);
+    const mainMenu = document.getElementById('mainMenu');
+        const menuAddCal = document.createElement('button');
+        mainMenu.appendChild(menuAddCal);
+        menuAddCal.className ='menuBtn';
+        menuAddCal.id = 'menuAddCal';
+        menuAddCal.innerHTML ="Califications"
+            const img1 = document.createElement('img');
+            menuAddCal.appendChild(img1);
             img1.className ='menuImg';
             img1.src = imgcalifications;
-        let menu2 = document.createElement('button');
-        mainMenu.appendChild(menu2);
-        menu2.className ='menuBtn';
-        menu2.id ='menu2';
-        menu2.innerHTML ="Examn";
-            let img2 = document.createElement('img');
-            menu2.appendChild(img2);
+        const menuAddExm = document.createElement('button');
+        mainMenu.appendChild(menuAddExm);
+        menuAddExm.className ='menuBtn';
+        menuAddExm.id ='menuAddExm';
+        menuAddExm.innerHTML ="Examn";
+            const img2 = document.createElement('img');
+            menuAddExm.appendChild(img2);
             img2.className ='menuImg';
             img2.src = imgtask;
-        let menu3 = document.createElement('button');
-        mainMenu.appendChild(menu3);
-        menu3.className ='menuBtn';
-        menu3.id = 'menu3';
-        menu3.innerHTML ="Homework"
-            let img3 = document.createElement('img');
-            menu3.appendChild(img3);
+        const menuAddHw = document.createElement('button');
+        mainMenu.appendChild(menuAddHw);
+        menuAddHw.className ='menuBtn';
+        menuAddHw.id = 'menuAddHw';
+        menuAddHw.innerHTML ="Homework"
+            const img3 = document.createElement('img');
+            menuAddHw.appendChild(img3);
             img3.className ='menuImg';
             img3.src = imgtask;
-        let menu4 = document.createElement('button');
-        mainMenu.appendChild(menu4);
-        menu4.className = 'menuBtn';
-        menu4.id = 'menu4';
-        menu4.innerHTML ="Notes";
-            let img4 = document.createElement('img');
-            menu4.appendChild(img4);
+        const menuAddNot = document.createElement('button');
+        mainMenu.appendChild(menuAddNot);
+        menuAddNot.className = 'menuBtn';
+        menuAddNot.id = 'menuAddNot';
+        menuAddNot.innerHTML ="Notes";
+            const img4 = document.createElement('img');
+            menuAddNot.appendChild(img4);
             img4.className ='menuImg';
             img4.src = imgcoment;
-        let menu5 = document.createElement('button');
-        mainMenu.appendChild(menu5);
-        menu5.className ='menuBtn';
-        menu5.id = 'menu5';
-        menu5.innerHTML ="Reunion";
-            let img5 = document.createElement('img');
-            menu5.appendChild(img5);
+        const menuAddMet = document.createElement('button');
+        mainMenu.appendChild(menuAddMet);
+        menuAddMet.className ='menuBtn';
+        menuAddMet.id = 'menuAddMet';
+        menuAddMet.innerHTML ="Meeting";
+            const img5 = document.createElement('img');
+            menuAddMet.appendChild(img5);
             img5.className ='menuImg';
             img5.src = imgUsers;
-        let menu6 = document.createElement('button');
-        mainMenu.appendChild(menu6);
-        menu6.className ='menuBtn';
-        menu6.id ='menu6';
-        menu6.innerHTML ="Activity";
-            let img6 = document.createElement('img');
-            menu6.appendChild(img6);
+        const menuAddAct = document.createElement('button');
+        mainMenu.appendChild(menuAddAct);
+        menuAddAct.className ='menuBtn';
+        menuAddAct.id ='menuAddAct';
+        menuAddAct.innerHTML ="Activity";
+            const img6 = document.createElement('img');
+            menuAddAct.appendChild(img6);
             img6.className ='menuImg';
             img6.src = imgdashboard;
-        let menu7 = document.createElement('button');
-        mainMenu.appendChild(menu7);
-        menu7.className ='menuBtn';
-        menu7.id ='menu7';
-        menu7.innerHTML ="Money";
-            let img7 = document.createElement('img');
-            menu7.appendChild(img7);
+        const menuAddMon = document.createElement('button');
+        mainMenu.appendChild(menuAddMon);
+        menuAddMon.className ='menuBtn';
+        menuAddMon.id ='menuAddMon';
+        menuAddMon.innerHTML ="Money";
+            const img7 = document.createElement('img');
+            menuAddMon.appendChild(img7);
             img7.className ='menuImg';
             img7.src = imgmoney;
+
+    function AddCalifications() {
+        const btnAddCal = document.createElement('button');
+        mainWelcome.appendChild(btnAddCal);
+        btnAddCal.className='btnAddPost';
+        btnAddCal.innerHTML="Add Califications";
+            btnAddCal.addEventListener('click',()=>{
+                const contAddCal = document.createElement('div');
+                mainDashboard.appendChild(contAddCal);
+                contAddCal.className='contAddCal';
+                    let inpNameStudent = document.createElement('input');
+                    contAddCal.appendChild(inpNameStudent);
+                    inpNameStudent.type = 'text' ;
+                    inpNameStudent.placeholder = "Student" ;
+                    inpNameStudent.className = 'inpNameStudent' ;
+                    let inpCalStudent = document.createElement('input') ;
+                    contAddCal.appendChild(inpCalStudent) ;
+                    inpCalStudent.type = 'inpCalStudent' ;
+                    inpCalStudent.placeholder = "Calification Student" ;
+                    inpCalStudent.className = 'inpCalStudent' ;
+                    let inpRevStudent = document.createElement('input') ;
+                    contAddCal.appendChild(inpRevStudent) ;
+                    inpRevStudent.type = 'text' ;
+                    inpRevStudent.placeholder = "Add Reviw Student" ;
+                    inpRevStudent.className = 'inpRevStudent' ;
+            })
+    }
+
+    menuAddCal.addEventListener('click',()=>{
+
+    })
 }
